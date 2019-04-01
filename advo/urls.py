3e366484300f396c665cf22eeb8ee009f2e56160
Claire Fridkin <clairefridkin@college.harvard.edu>
2019-03-31 23:46:01 -0400

    comp updates

--- advo/urls.py
+++ advo/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     url(r'^contributor/(?P<author_id>[\d]+)/(?P<name>.*)/$', magazine.views.contributor_page),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^mce_filebrowser/', include('mce_filebrowser.urls')),
-    url(r'^search(?:/(?P<type_filter>[art|features|poetry|fiction|columns|notes|interviews]+))?/?$', magazine.views.FilterSearchView(), name='filter_search'),
+    url(r'^search(?:/(?P<type_filter>[art|features|poetry|fiction|columns|notes|interviews]+))?/?$', magazine.views.search_page, name='filter_search'),
     url(r'^blog/', include('blog.urls')),
     url(r'^shopSubmit$',payments.views.shopSubmit),
     url(r'^grappelli/', include('grappelli.urls')),
